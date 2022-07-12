21c782e57afb15700b7d91721b0ec2f6269a80d7
LukaFlores <luka.flores@mail.com>
2022-07-12 14:56:27 -0700

    chore: init

---
+++ src/firstOfArray.ts
@@ -0,0 +1,64 @@
+/*
+  14 - First of Array
+  -------
+  by Anthony Fu (@antfu) #easy #array
+  
+  ### Question
+  
+  Implement a generic `First<T>` that takes an Array `T` and returns it's first element's type.
+  
+  For example:
+  
+  ```ts
+  type arr1 = ['a', 'b', 'c']
+  type arr2 = [3, 2, 1]
+  
+  type head1 = First<arr1> // expected to be 'a'
+  type head2 = First<arr2> // expected to be 3
+  ```
+  
+  > View on GitHub: https://tsch.js.org/14
+*/
+
+
+/* _____________ Your Code Here _____________ */
+
+// First Solution
+//type First<T extends any[]> = T["length"] extends 0 ? never : T[0]
+
+//Infer
+// If this is an array return U (First Time) or Never
+// Using array destructing [infer U, ...any[]] U = type of first value
+type First<T extends any[]> = T extends [infer U, ...any[]] ? U : never
+
+// [any, ...any[]] says that there is more than 1 type
+//type First<T extends [any, ...any[]]> = T extends [infer U, ...any[]] ? U : never
+// Doesn't cover Never case
+
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  First<'notArray'>,
+  // @ts-expect-error
+  First<{ 0: 'arrayLike' }>,
+]
+
+
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/14/answer
+  > View solutions: https://tsch.js.org/14/solutions
+  > More Challenges: https://tsch.js.org
+*/
+
